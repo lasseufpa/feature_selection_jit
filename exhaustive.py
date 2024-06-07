f80b561da0066331b7737bbd09d85f8d9befc4ad
Marco Maues <marcomauesjr@gmail.com>
2024-06-07 12:53:20 -0300

    Método Exaustivo de seleção de features usando como modelo a regressão logistica

--- exhaustive.py
+++ exhaustive.py
@@ -5,35 +5,48 @@ from sklearn.model_selection import train_test_split
 from mlxtend.feature_selection import ExhaustiveFeatureSelector as EFS
 import pandas as pd
 
-data = pd.read_csv('src/features.csv')
-X = data.drop(columns=['label','commit'])
-Y = data['label']
+
+def exhaustive(X_train, Y_train):
+    model = LogisticRegression(max_iter=1000,random_state=42)
+    smote = SMOTE(random_state=42)
+    pipeline = Pipeline([('SMOTE', smote), ('Logistic Regression', model)])
+    efs = EFS(pipeline, min_features=1,
+            max_features=12,
+            scoring='roc_auc',
+            cv=5,
+            n_jobs=1)
+    efs = efs.fit(X_train, Y_train)
+    return efs
 
 
-# Divida o dataset em conjunto de treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
-# Defina o modelo
-model = LogisticRegression(max_iter=1000, random_state=42)
+def main():
 
-# Defina o método de oversampling
-smote = SMOTE(random_state=42)
+    data = pd.read_csv('src/features.csv')
+    X = data.drop(columns=['label','commit'])
+    Y = data['label']
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
-# Crie uma pipeline com o método de oversampling e o modelo
-pipeline = Pipeline([('SMOTE', smote), ('Logistic Regression', model)])
+    # Defina o modelo
+    model = LogisticRegression(max_iter=1000,random_state=42)
+    # Defina o método de oversampling
+    smote = SMOTE(random_state=42)
 
-# Defina o seletor de recursos
-efs = EFS(pipeline, min_features=1, max_features=12, scoring='roc_auc', cv=5,n_jobs=1)
-efs = efs.fit(X_train, y_train)
+    # Crie uma pipeline com o método de oversampling e o modelo
+    pipeline = Pipeline([('SMOTE', smote), ('Logistic Regression', model)])
 
-# Imprima as melhores características encontradas
-print('Best features:', efs.best_idx_)
-print('Best features names:', efs.best_feature_names_)
-print(f'Best score: {efs.best_score_}')
+    # Defina o seletor de recursos
+    efs = EFS(model, min_features=1,
+              max_features=1,
+              scoring='roc_auc',
+              cv=5,
+              n_jobs=1)
 
-# Avalie o desempenho no conjunto de teste usando as melhores características (opcional)
-# X_train_selected = X_train.iloc[:, list(efs.best_idx_)]
-# X_test_selected = X_test.iloc[:, list(efs.best_idx_)]
-# pipeline.fit(X_train_selected, y_train)
-# test_score = pipeline.score(X_test_selected, y_test)
-# print('Test score (roc_auc):', test_score)
+    efs = efs.fit(X_train, y_train)
+
+    efs_df = pd.DataFrame.from_dict(efs.get_metric_dict()).T
+    efs_df = efs_df.sort_values('avg_score', ascending=False,ignore_index=True)
+    efs_df.to_csv('src/Results/LogisticRegression/efs_roc_auc.csv')
+
+if __name__ == '__main__':
+    main()
