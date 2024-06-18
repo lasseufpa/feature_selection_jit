8e297aaaa110fafd779a2d320f894a4cfa1de8dd
Marco Maues <marcomauesjr@gmail.com>
2024-06-18 17:39:52 -0300

    Inserção dos testes da RL, DT com smote e sem smote e utilizando as melhores features

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_sco
 from imblearn.pipeline import make_pipeline
 from sklearn.model_selection import StratifiedKFold
 from imblearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
 
 
 def pipeline_smote(X,Y):
@@ -175,6 +176,7 @@ def logistic_regression(X,Y):
                                      'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
     metrics_df.to_csv('src/Results/LogisticRegression/metrics.csv', index =  False)
 
+
 def logistic_regression_smote(X,Y):
     model = LogisticRegression(max_iter=1000,random_state=42)
     smote = SMOTE(random_state=42)
@@ -205,11 +207,206 @@ def logistic_regression_smote(X,Y):
                                      'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
     metrics_df.to_csv('src/Results/LogisticRegression/metrics_smote.csv', index =  False)
 
+
 def cross_validation_logistic_regression(X,Y, top_k_features):
-    print(top_k_features)
+    X=X[top_k_features]
+    model = LogisticRegression(max_iter=1000,random_state=42)
+    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+
+    counter = 1
+    for train_index, test_index in cv.split(X, Y):
+        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
+        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
+        model.fit(X_train_cv, Y_train_cv)
+        y_pred = model.predict(X_test_cv)
+        metrics_df = metrics_df._append({'Fold': counter,
+                                         'Precision': precision_score(Y_test_cv, y_pred),
+                                         'Recall':recall_score(Y_test_cv, y_pred),
+                                         'F1-score':f1_score(Y_test_cv, y_pred),
+                                         'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
+        counter += 1
+    metrics_df = metrics_df._append({'Fold': 'Mean',
+                                     'Precision': metrics_df['Precision'].mean(),
+                                     'Recall': metrics_df['Recall'].mean(),
+                                     'F1-score': metrics_df['F1-score'].mean(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
+    metrics_df = metrics_df._append({'Fold': 'Std',
+                                     'Precision': metrics_df['Precision'].std(),
+                                     'Recall': metrics_df['Recall'].std(),
+                                     'F1-score': metrics_df['F1-score'].std(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
+
+    metrics_df.to_csv('src/Results/LogisticRegression/metrics_best_features.csv', index =  False)
+
 
 def cross_validation_logistic_regression_smote(X,Y, top_k_features):
-    print(top_k_features)
+    X=X[top_k_features]
+    model = LogisticRegression(max_iter=1000,random_state=42)
+    smote = SMOTE(random_state=42)
+    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    pipeline = make_pipeline(smote, model)
+    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+
+    counter = 1
+    for train_index, test_index in cv.split(X, Y):
+        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
+        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
+        pipeline.fit(X_train_cv, Y_train_cv)
+        y_pred = model.predict(X_test_cv)
+        metrics_df = metrics_df._append({'Fold': counter,
+                                         'Precision': precision_score(Y_test_cv, y_pred),
+                                         'Recall':recall_score(Y_test_cv, y_pred),
+                                         'F1-score':f1_score(Y_test_cv, y_pred),
+                                         'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
+        counter += 1
+    metrics_df = metrics_df._append({'Fold': 'Mean',
+                                     'Precision': metrics_df['Precision'].mean(),
+                                     'Recall': metrics_df['Recall'].mean(),
+                                     'F1-score': metrics_df['F1-score'].mean(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
+    metrics_df = metrics_df._append({'Fold': 'Std',
+                                     'Precision': metrics_df['Precision'].std(),
+                                     'Recall': metrics_df['Recall'].std(),
+                                     'F1-score': metrics_df['F1-score'].std(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
+
+    metrics_df.to_csv('src/Results/LogisticRegression/metrics_best_features_smote.csv', index =  False)
+
+
+def decision_tree_cross_validation_smote(X,Y):
+    model = DecisionTreeClassifier(random_state=42)
+    smote = SMOTE(random_state=42)
+    pipeline = make_pipeline(smote, model)
+    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+
+    counter = 1
+    for train_index, test_index in cv.split(X, Y):
+        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
+        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
+        pipeline.fit(X_train_cv, Y_train_cv)
+        y_pred = pipeline.predict(X_test_cv)
+        features_importance = pipeline.named_steps['decisiontreeclassifier'].feature_importances_
+        features_importance = pd.DataFrame(features_importance, index=X.columns, columns=['importance'])
+        features_importance = features_importance.sort_values(by='importance', ascending=False)
+        features_importance.to_csv(f'src/Results/DecisionTreeClassifier/features_importance/features_importance_{counter}.csv')
+        metrics_df = metrics_df._append({'Fold': counter,
+                        'Precision': precision_score(Y_test_cv, y_pred),
+                        'Recall':recall_score(Y_test_cv, y_pred),
+                        'F1-score':f1_score(Y_test_cv, y_pred),
+                        'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
+        counter += 1
+    metrics_df = metrics_df._append({'Fold': 'Mean',
+                                     'Precision': metrics_df['Precision'].mean(),
+                                     'Recall': metrics_df['Recall'].mean(),
+                                     'F1-score': metrics_df['F1-score'].mean(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
+    metrics_df = metrics_df._append({'Fold': 'Std',
+                                     'Precision': metrics_df['Precision'].std(),
+                                     'Recall': metrics_df['Recall'].std(),
+                                     'F1-score': metrics_df['F1-score'].std(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
+    
+    metrics_df.to_csv('src/Results/DecisionTreeClassifier/metrics_smote.csv', index =  False)
+
+
+def decision_tree_cross_validation(X,Y):
+    model = DecisionTreeClassifier(random_state=42)
+    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+
+    counter = 1
+    for train_index, test_index in cv.split(X, Y):
+        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
+        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
+        model.fit(X_train_cv, Y_train_cv)
+        y_pred = model.predict(X_test_cv)
+        metrics_df = metrics_df._append({'Fold': counter,
+                        'Precision': precision_score(Y_test_cv, y_pred),
+                        'Recall':recall_score(Y_test_cv, y_pred),
+                        'F1-score':f1_score(Y_test_cv, y_pred),
+                        'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
+        counter += 1
+    metrics_df = metrics_df._append({'Fold': 'Mean',
+                                     'Precision': metrics_df['Precision'].mean(),
+                                     'Recall': metrics_df['Recall'].mean(),
+                                     'F1-score': metrics_df['F1-score'].mean(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
+    metrics_df = metrics_df._append({'Fold': 'Std',
+                                     'Precision': metrics_df['Precision'].std(),
+                                     'Recall': metrics_df['Recall'].std(),
+                                     'F1-score': metrics_df['F1-score'].std(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
+    
+    metrics_df.to_csv('src/Results/DecisionTreeClassifier/metrics.csv', index =  False)
+ 
+
+def decision_tree_cross_validation_top_k_features(X,Y, top_k_features):
+    X = X[top_k_features]
+    model = DecisionTreeClassifier(random_state=42)
+    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+
+    counter = 1
+    for train_index, test_index in cv.split(X, Y):
+        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
+        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
+        model.fit(X_train_cv, Y_train_cv)
+        y_pred = model.predict(X_test_cv)
+        metrics_df = metrics_df._append({'Fold': counter,
+                        'Precision': precision_score(Y_test_cv, y_pred),
+                        'Recall':recall_score(Y_test_cv, y_pred),
+                        'F1-score':f1_score(Y_test_cv, y_pred),
+                        'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
+        counter += 1
+    metrics_df = metrics_df._append({'Fold': 'Mean',
+                                     'Precision': metrics_df['Precision'].mean(),
+                                     'Recall': metrics_df['Recall'].mean(),
+                                     'F1-score': metrics_df['F1-score'].mean(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
+    metrics_df = metrics_df._append({'Fold': 'Std',
+                                     'Precision': metrics_df['Precision'].std(),
+                                     'Recall': metrics_df['Recall'].std(),
+                                     'F1-score': metrics_df['F1-score'].std(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
+    
+    metrics_df.to_csv('src/Results/DecisionTreeClassifier/metrics_best_features.csv', index =  False)
+
+
+def decision_tree_cross_validation_top_k_features_smote(X,Y, top_k_features):
+    X = X[top_k_features]
+    smote = SMOTE(random_state=42)
+    model = DecisionTreeClassifier(random_state=42)
+    pipeline = make_pipeline(smote, model)
+    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+
+    counter = 1
+    for train_index, test_index in cv.split(X, Y):
+        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
+        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
+        pipeline.fit(X_train_cv, Y_train_cv)
+        y_pred = pipeline.predict(X_test_cv)
+        metrics_df = metrics_df._append({'Fold': counter,
+                        'Precision': precision_score(Y_test_cv, y_pred),
+                        'Recall':recall_score(Y_test_cv, y_pred),
+                        'F1-score':f1_score(Y_test_cv, y_pred),
+                        'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
+        counter += 1
+    metrics_df = metrics_df._append({'Fold': 'Mean',
+                                     'Precision': metrics_df['Precision'].mean(),
+                                     'Recall': metrics_df['Recall'].mean(),
+                                     'F1-score': metrics_df['F1-score'].mean(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
+    metrics_df = metrics_df._append({'Fold': 'Std',
+                                     'Precision': metrics_df['Precision'].std(),
+                                     'Recall': metrics_df['Recall'].std(),
+                                     'F1-score': metrics_df['F1-score'].std(),
+                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
+    
+    metrics_df.to_csv('src/Results/DecisionTreeClassifier/metrics_best_features_smote.csv', index =  False)
+
 
 def main():
     parse = argparse.ArgumentParser(description="Dataset of the features")
@@ -225,13 +422,34 @@ def main():
     
     X = data.drop(columns=['commit','label'])
     Y = data['label']
-    ranking = pd.read_json('src/Results/RandomForest/ranking_medio.json')
-    top_k_features = ranking[:4]
-    top_k_features = top_k_features[0].to_list()
-    pipeline_smote(X,Y)
-    pipeline_no_smote(X,Y)
-    cross_validation(X,Y, top_k_features)
-    cross_validation_smote(X,Y, top_k_features)
+
+    ####################### Random Forest ############################
+    
+    # ranking = pd.read_json('src/Results/RandomForest/ranking_medio.json')
+    # top_k_features = ranking[:4]
+    # top_k_features = top_k_features[0].to_list()
+    # pipeline_smote(X,Y)
+    # pipeline_no_smote(X,Y)
+    # cross_validation(X,Y, top_k_features)
+    # cross_validation_smote(X,Y, top_k_features)
+    
+    ######################## Logistic Regression, Top 1 features ######################
+    # top_k_features = ['modified_subsystems', 'entropy', 'number_of_authors','purpose', 'number_unique_changes', 'lines_of_code_added', 'lines_of_code_deleted', 'line_of_code_old']
+    # print(top_k_features)
+    # logistic_regression(X,Y)
+    # logistic_regression_smote(X,Y)
+    # cross_validation_logistic_regression(X,Y, top_k_features)
+    # cross_validation_logistic_regression_smote(X,Y, top_k_features)
+
+    ######################## Decision Tree ######################
+    # decision_tree_cross_validation(X,Y)
+    # decision_tree_cross_validation_smote(X,Y)
+    top_k_features = ['number_unique_changes','lines_of_code_added', 'number_of_authors', 'line_of_code_old']
+    # decision_tree_cross_validation_top_k_features(X,Y, top_k_features)
+    decision_tree_cross_validation_top_k_features_smote(X,Y, top_k_features)
+
 
 if __name__ == "__main__":
     main()
+
+    
