e228c6ef8d22b66d9cfcd67b0c5e25ff6a1d4b90
Marco Maues <marcomauesjr@gmail.com>
2024-06-19 16:04:01 -0300

    Funções suporte para main

--- util/RandomForest.py
+++ util/RandomForest.py
@@ -4,32 +4,60 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score
 from imblearn.pipeline import make_pipeline
 from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
 
 
 def random_forest_all_feature_smote(X,Y):
     model = RandomForestClassifier(n_estimators=500, random_state=42,max_features='log2',max_depth=90,
                                min_samples_split=5, min_samples_leaf=4, criterion='entropy')
-    smote = SMOTE(random_state=42)
-    pipeline = make_pipeline(smote, model)
-    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
-    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+    save_metrics(X = X,Y = Y,model = model,smote=True)
 
+
+def random_forest_all_feature(X,Y):
+    model = RandomForestClassifier(n_estimators=500, random_state=42,max_features='log2',max_depth=90,
+                               min_samples_split=5, min_samples_leaf=4, criterion='entropy')
+    save_metrics(X,Y,model,save_features_importance=True)
+
+def random_forest_top_feature(X,Y, top_features):
+    X = X[top_features]
+    model = RandomForestClassifier(n_estimators=500, random_state=42,max_features='log2',max_depth=90,
+                               min_samples_split=5, min_samples_leaf=4)
+    save_metrics(X,Y,model,top_features=top_features)
+
+def random_forest_top_feature_smote(X,Y, top_features):
+    X = X[top_features]
+    model = RandomForestClassifier(n_estimators=500, random_state=42,max_features='log2',max_depth=90,
+                               min_samples_split=5, min_samples_leaf=4, criterion='entropy')
+    save_metrics(X = X ,Y = Y,model = model,smote = True,top_features = top_features)
+
+
+def save_metrics(X,Y,model,smote=False,top_features=None):
+    if top_features:
+        X = X[top_features]
+    if smote:
+        smote = SMOTE(random_state=42)
+        pipeline = make_pipeline(smote, model)
+    else:
+        pipeline = model
+    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    # metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+    
     counter = 1
     for train_index, test_index in cv.split(X, Y):
         X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
         Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
         pipeline.fit(X_train_cv, Y_train_cv)
         y_pred = pipeline.predict(X_test_cv)
-        # features_importance = pipeline.named_steps['randomforestclassifier'].feature_importances_
-        # features_importance = pd.DataFrame(features_importance, index=X.columns, columns=['importance'])
-        # features_importance = features_importance.sort_values(by='importance', ascending=False)
-        # features_importance.to_csv(f'src/Results/RandomForest/features_importance_{counter}.csv')
         metrics_df = metrics_df._append({'Fold': counter,
                         'Precision': precision_score(Y_test_cv, y_pred),
                         'Recall':recall_score(Y_test_cv, y_pred),
                         'F1-score':f1_score(Y_test_cv, y_pred),
                         'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
+        # feature_importance = pipeline.named_steps['randomforestclassifier'].feature_importances_
+        # feature_importance = pd.DataFrame(feature_importance, index = X.columns, columns=['importance']).sort_values('importance', ascending=False)
+        # feature_importance.to_csv(f'src/Results/RandomForest/features_importances/features_importance_{counter}.csv', index =  True)
         counter += 1
+
     metrics_df = metrics_df._append({'Fold': 'Mean',
                                      'Precision': metrics_df['Precision'].mean(),
                                      'Recall': metrics_df['Recall'].mean(),
@@ -41,104 +69,14 @@ def random_forest_all_feature_smote(X,Y):
                                      'F1-score': metrics_df['F1-score'].std(),
                                      'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
     
-    metrics_df.to_csv('src/Results/RandomForest/metrics_smote.csv', index =  False)
+    if smote and top_features:
+        metrics_df.to_csv('src/Results/RandomForest/metrics_best_features_smote.csv', index =  False)
 
+    elif top_features:
+        metrics_df.to_csv('src/Results/RandomForest/metrics_best_features.csv', index =  False)
 
-def random_forest_all_feature(X,Y):
-    model = RandomForestClassifier(n_estimators=500, random_state=42,max_features='log2',max_depth=90,
-                               min_samples_split=5, min_samples_leaf=4, criterion='entropy')
-    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
-    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
+    elif smote:
+        metrics_df.to_csv('src/Results/RandomForest/metrics_smote.csv', index =  False)
 
-    counter = 1
-    for train_index, test_index in cv.split(X, Y):
-        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
-        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
-        model.fit(X_train_cv, Y_train_cv)
-        y_pred = model.predict(X_test_cv)
-        metrics_df = metrics_df._append({'Fold': counter,
-                        'Precision': precision_score(Y_test_cv, y_pred),
-                        'Recall':recall_score(Y_test_cv, y_pred),
-                        'F1-score':f1_score(Y_test_cv, y_pred),
-                        'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
-        counter += 1
-    metrics_df = metrics_df._append({'Fold': 'Mean',
-                                     'Precision': metrics_df['Precision'].mean(),
-                                     'Recall': metrics_df['Recall'].mean(),
-                                     'F1-score': metrics_df['F1-score'].mean(),
-                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
-    metrics_df = metrics_df._append({'Fold': 'Std',
-                                     'Precision': metrics_df['Precision'].std(),
-                                     'Recall': metrics_df['Recall'].std(),
-                                     'F1-score': metrics_df['F1-score'].std(),
-                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
-    
-    metrics_df.to_csv('src/Results/RandomForest/metrics.csv', index =  False)
-
-
-def random_forest_top_feature(X,Y, top_features):
-    X = X[top_features]
-    model = RandomForestClassifier(n_estimators=500, random_state=42,max_features='log2',max_depth=90,
-                               min_samples_split=5, min_samples_leaf=4)
-    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
-    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
-    
-    counter = 1
-    for train_index, test_index in cv.split(X, Y):
-        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
-        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
-        model.fit(X_train_cv, Y_train_cv)
-        y_pred = model.predict(X_test_cv)
-        metrics_df = metrics_df._append({'Fold': counter,
-                        'Precision': precision_score(Y_test_cv, y_pred),
-                        'Recall':recall_score(Y_test_cv, y_pred),
-                        'F1-score':f1_score(Y_test_cv, y_pred),
-                        'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
-        counter += 1
-    metrics_df = metrics_df._append({'Fold': 'Mean',
-                                     'Precision': metrics_df['Precision'].mean(),
-                                     'Recall': metrics_df['Recall'].mean(),
-                                     'F1-score': metrics_df['F1-score'].mean(),
-                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
-    metrics_df = metrics_df._append({'Fold': 'Std',
-                                     'Precision': metrics_df['Precision'].std(),
-                                     'Recall': metrics_df['Recall'].std(),
-                                     'F1-score': metrics_df['F1-score'].std(),
-                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
-    
-    metrics_df.to_csv('src/Results/RandomForest/metrics_best_features.csv', index =  False)
-
-
-def random_forest_top_feature_smote(X,Y, top_features):
-    X = X[top_features]
-    smote = SMOTE(random_state=42)
-    model = RandomForestClassifier(n_estimators=500, random_state=42,max_features='log2',max_depth=90,
-                               min_samples_split=5, min_samples_leaf=4, criterion='entropy')
-    cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
-    pipeline = make_pipeline(smote, model)
-    metrics_df = pd.DataFrame(columns=['Fold', 'Precision', 'Recall', 'F1-score', 'Roc_auc_score'])
-    
-    counter = 1
-    for train_index, test_index in cv.split(X, Y):
-        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
-        Y_train_cv, Y_test_cv = Y.iloc[train_index], Y.iloc[test_index]
-        pipeline.fit(X_train_cv, Y_train_cv)
-        y_pred = model.predict(X_test_cv)
-        metrics_df = metrics_df._append({'Fold': counter,
-                        'Precision': precision_score(Y_test_cv, y_pred),
-                        'Recall':recall_score(Y_test_cv, y_pred),
-                        'F1-score':f1_score(Y_test_cv, y_pred),
-                        'Roc_auc_score':roc_auc_score(Y_test_cv,y_pred)}, ignore_index=True)
-        counter += 1
-    metrics_df = metrics_df._append({'Fold': 'Mean',
-                                     'Precision': metrics_df['Precision'].mean(),
-                                     'Recall': metrics_df['Recall'].mean(),
-                                     'F1-score': metrics_df['F1-score'].mean(),
-                                     'Roc_auc_score': metrics_df['Roc_auc_score'].mean()}, ignore_index=True)
-    metrics_df = metrics_df._append({'Fold': 'Std',
-                                     'Precision': metrics_df['Precision'].std(),
-                                     'Recall': metrics_df['Recall'].std(),
-                                     'F1-score': metrics_df['F1-score'].std(),
-                                     'Roc_auc_score': metrics_df['Roc_auc_score'].std()}, ignore_index=True)
-    
-    metrics_df.to_csv('src/Results/RandomForest/metrics_best_features_smote.csv', index =  False)
+    else:
+        metrics_df.to_csv('src/Results/RandomForest/metrics.csv', index =  False)
