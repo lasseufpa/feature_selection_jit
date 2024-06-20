ad2792c9556c3107287188b46defc5e17d7028b7
Marco Maues <marcomauesjr@gmail.com>
2024-06-20 18:08:23 -0300

    Código para verificar as métricas dos arquivos

--- image.py
+++ image.py
@@ -47,57 +47,45 @@ import pandas as pd
 # plot_metric(folds, roc_auc, mean_roc_auc, std_roc_auc, 'Roc_auc_score')
 
 from tabulate import tabulate
+import glob
 
-# Dados para a tabela
-# metrics = {
-#     "Precision": (0.12113711515809844,0.005383411805702218),
-#     "Recall": (0.42797927461139895, 0.022844982057045177),
-#     "F1-score": (0.18882451994534027, 0.008760824822727525),
-#     "Roc_auc_score": (0.6691717428531841, 0.011278651591829133)
-# }
+files = glob.glob("src/Results/RandomForest/metrics_*normalized.csv")
+files.sort()
+for file in files:
+    df = pd.read_csv(file)
+    mean_precision = df['Precision'][5]
+    std_precision = df['Precision'][6]
+
+    mean_recall = df['Recall'][5]
+    std_recall = df['Recall'][6]
+
+    mean_f1_score = df['F1-score'][5]
+    std_f1_score = df['F1-score'][6]
+
+    mean_roc_auc = df['Roc_auc_score'][5]
+    std_roc_auc = df['Roc_auc_score'][6]
+    print(f'\n============================= {file.split("/")[-1].split(".")[0]} ==================================')
+    print(tabulate([['Precision', f"{mean_precision:.3f} ± {std_precision:.3f}"],
+                    ['Recall', f"{mean_recall:.3f} ± {std_recall:.3f}"],
+                    ['F1-score', f"{mean_f1_score:.3f} ± {std_f1_score:.3f}"],
+                    ['Roc_auc_score', f"{mean_roc_auc:.3f} ± {std_roc_auc:.3f}"]]))
 
 
-# table = []
-# for metric, (mean, std) in metrics.items():
-#     value = f"{mean:.3f} ± {std:.3f}"
-#     table.append([metric, value])
 
-# Imprimir a tabela
-# print(tabulate(table, headers=["Metric"], tablefmt="rounded_grid"))
-
-# df = pd.read_csv('src/Results/LogisticRegression/metrics_best_features.csv')
-
-# # Separar os dados dos folds (excluindo as últimas duas linhas)
+# Separar os dados dos folds (excluindo as últimas duas linhas)
 # folds = df['Fold'][:-2].astype(float)
 # precision = df['Precision'][:-2].astype(float)
 # recall = df['Recall'][:-2].astype(float)
 # f1_score = df['F1-score'][:-2].astype(float)
 # roc_auc = df['Roc_auc_score'][:-2].astype(float)
 
-# # Obter médias e desvios padrão das últimas duas linhas
-# mean_precision = df['Precision'][5]
-# std_precision = df['Precision'][6]
+# Obter médias e desvios padrão das últimas duas linhas
 
-# mean_recall = df['Recall'][5]
-# std_recall = df['Recall'][6]
+# import pandas as pd
+# import glob
 
-# mean_f1_score = df['F1-score'][5]
-# std_f1_score = df['F1-score'][6]
-
-# mean_roc_auc = df['Roc_auc_score'][5]
-# std_roc_auc = df['Roc_auc_score'][6]
-
-# print(tabulate([['Precision', f"{mean_precision:.3f} ± {std_precision:.3f}"],
-#                 ['Recall', f"{mean_recall:.3f} ± {std_recall:.3f}"],
-#                 ['F1-score', f"{mean_f1_score:.3f} ± {std_f1_score:.3f}"],
-#                 ['Roc_auc_score', f"{mean_roc_auc:.3f} ± {std_roc_auc:.3f}"]]))
-
-import pandas as pd
-import glob
-
-# Obter todos os arquivos com informações de classificação
-files = glob.glob('src/Results/features_importance/features_importance_?*')
-# print(f"Arquivos encontrados: {files}")
+# # Obter todos os arquivos com informações de classificação
+files = glob.glob('src/Results/RandomForest/features_importances/features_importance_?_normalized.csv')
 
 # Inicializar um dicionário para armazenar os ranks dos itens
 item_ranks = {}
